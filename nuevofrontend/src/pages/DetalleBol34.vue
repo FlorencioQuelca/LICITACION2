4d4a23ff37876777cc3f0edb396db5231f0de0a3
FlorencioQuelca <florens.lonwe@gmail.com>
2022-09-09 18:06:20 -0400

    correcciones

--- nuevofrontend/src/pages/DetalleBol34.vue
+++ nuevofrontend/src/pages/DetalleBol34.vue
@@ -1942,8 +1942,8 @@ export default {
                }else{
                      descargarnombre=this.dato.codigo+"_NO_CUMPLE"+".pdf"
                }
-            //doc.save(descargarnombre);
-             window.open(doc.output('bloburl',{filename:descargarnombre}), '_blank');
+            doc.save(descargarnombre);
+           //  window.open(doc.output('bloburl',{filename:descargarnombre}), '_blank');
       },
       isBoldOpen (arrayLength, valueBefore = false) {
     const isEven = arrayLength % 2 === 0;
@@ -2451,6 +2451,7 @@ export default {
                 }
 
              window.open(doc.output('bloburl',{filename:"FICHA.pdf"}), '_blank');
+             this.$q.loading.hide()
              })
 
 
