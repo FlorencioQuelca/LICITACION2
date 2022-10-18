ab2d7c3abe7970ffc6589f3b879bd9ce236cfd47
FlorencioQuelca <florens.lonwe@gmail.com>
2022-10-18 14:14:51 -0400

    subida de -quasar

---
+++ nuevofrontend/.quasar/quasar-user-options.js
@@ -0,0 +1,22 @@
+/**
+ * THIS FILE IS GENERATED AUTOMATICALLY.
+ * DO NOT EDIT.
+ *
+ * You are probably looking on adding startup/initialization code.
+ * Use "quasar new boot <name>" and add it there.
+ * One boot file per concern. Then reference the file(s) in quasar.conf.js > boot:
+ * boot: ['file', ...] // do not add ".js" extension to it.
+ *
+ * Boot files are your "main.js"
+ **/
+
+import lang from 'quasar/lang/es'
+
+
+
+import {Loading,Notify,Dialog} from 'quasar'
+
+
+
+export default { config: {"loading":{},"notify":{}},lang,plugins: {Loading,Notify,Dialog} }
+
